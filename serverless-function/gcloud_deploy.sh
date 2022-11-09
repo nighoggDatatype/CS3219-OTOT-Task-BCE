16ec648195759858d02cfd9c62fe7384ff5ec898
nighoggDatatype <hosherman1965@gmail.com>
2022-11-09 21:48:29 +0800

    Improve notes on serverless function deployment

--- serverless-function/gcloud_deploy.sh
+++ serverless-function/gcloud_deploy.sh
@@ -1,7 +1,9 @@
 # Note that `asia-southeast1` is the Singapore region as of 6 Nov 2022
 # 
 # NOTE: Run this script in the folder `serverless-function`
-# NOTE: Run gcloud init first
+# NOTE: Beforehand, run one of the following first:
+#       - `gcloud init`, if this is the first time setting up gcloud.
+#       - `gcloud auth login`, if you have been logged out
 #
 # Deploy function
 gcloud functions deploy nodejs-http-function --gen2 --runtime=nodejs16 --region="asia-southeast1" --source=. --entry-point=serverlessFunction --trigger-http --allow-unauthenticated
